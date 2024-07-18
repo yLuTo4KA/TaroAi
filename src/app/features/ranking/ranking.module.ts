f4f441b03fdd082c90868f9df54b6abc18fed91e
Hryhorii Avcharov <h.avcharov@danieli.com>
2024-07-18 11:40:55 +0200

    Ranking page basic layout

--- src/app/features/ranking/ranking.module.ts
+++ src/app/features/ranking/ranking.module.ts
@@ -4,6 +4,7 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { RankingRoutes } from './ranking.routing.module';
 import { RankingListComponent } from './components/ranking-list/ranking-list.component';
 import { RankingPageComponent } from './pages/ranking-page/ranking-page.component';
+import { RankingTopComponent } from './components/ranking-top/ranking-top.component';
 
 @NgModule({
   imports: [
@@ -11,6 +12,6 @@ import { RankingPageComponent } from './pages/ranking-page/ranking-page.componen
     SharedModule,
     RankingRoutes
   ],
-  declarations: [RankingListComponent, RankingPageComponent]
+  declarations: [RankingListComponent, RankingPageComponent,RankingTopComponent]
 })
 export class RankingModule { }
