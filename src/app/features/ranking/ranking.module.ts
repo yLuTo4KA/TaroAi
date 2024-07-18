ed14487cf5973cdb520ffd1c237fc7107b9903b7
Hryhorii Avcharov <h.avcharov@danieli.com>
2024-07-18 11:06:38 +0200

    Ranking list template

--- src/app/features/ranking/ranking.module.ts
+++ src/app/features/ranking/ranking.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { RankingRoutes } from './ranking.routing.module';
+import { RankingListComponent } from './components/ranking-list/ranking-list.component';
+import { RankingPageComponent } from './pages/ranking-page/ranking-page.component';
 
 @NgModule({
   imports: [
@@ -9,6 +11,6 @@ import { RankingRoutes } from './ranking.routing.module';
     SharedModule,
     RankingRoutes
   ],
-  declarations: []
+  declarations: [RankingListComponent, RankingPageComponent]
 })
 export class RankingModule { }
