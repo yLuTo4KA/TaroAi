ebe7ec1984547b5c01f86571a9f6c697a7df9cca
Danil Hatin <kipsa25@mail.ru>
2024-08-26 22:37:37 +0500

    fix styles and footer

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
-    production: false,
+    production: true,
     initData: "user=%7B%22id%22%3A901201138%2C%22first_name%22%3A%22yLuTo4KA%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22yLuTo4KA%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%7D&chat_instance=5336847486606326250&chat_type=sender&auth_date=1724690837&hash=75a0623d02e6ed82d60106ac2a53eff67ae52bd932bf8a22cc498069a43ba796",
     baseUrl: 'https://taroai-546ac6a4db3b.herokuapp.com',
 }
