06a5a9313be55f8bda40ed9781834802649b9af4
Danil Hatin <kipsa25@mail.ru>
2024-08-30 19:12:26 +0500

    fix webhook

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 // bot.telegram.setWebhook('https://taroai-546ac6a4db3b.herokuapp.com/payment/status');
-app.use(await bot.createWebhook({ domain: 'https://taroai-546ac6a4db3b.herokuapp.com/payment/status' }));
+
 
 const expToken = '1d';
 
@@ -30,7 +30,14 @@ const expToken = '1d';
 bot.start((ctx) => ctx.reply('welcome'));
 bot.command('start', (ctx) => ctx.reply('ping'));
 
-bot.launch();
+bot.launch(
+    {
+        webhook: {
+            domain: 'https://taroai-546ac6a4db3b.herokuapp.com/payment/status',
+            port: 88
+        }
+    }
+);
 
 
 app.get('/', (req, res) => {
